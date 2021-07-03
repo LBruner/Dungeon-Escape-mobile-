cb44ae293fdf07142a25f1a72a105489087fa599
LBruner <leandrobruner15@gmail.com>
2021-07-03 19:46:00 -0300

    video 10-11-12-13

--- Dungeon Escape Mobile/Assets/Scripts/PlayerController.cs
+++ Dungeon Escape Mobile/Assets/Scripts/PlayerController.cs
@@ -7,17 +7,31 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private Rigidbody2D rb = null;
 
     [SerializeField] float speed = 2f;
+    [SerializeField] float jumpForce = 5.0f;
 
-    void Start()
+    void FixedUpdate()
     {
-        
+        Movement();
     }
 
-    
-    void Update()
+    private void Movement()
     {
         float horizontalInput = Input.GetAxisRaw("Horizontal");
 
-        rb.velocity = new Vector2(horizontalInput, rb.velocity.y) * speed * Time.deltaTime;
+        rb.velocity = new Vector2(horizontalInput * speed * Time.deltaTime, rb.velocity.y);
+
+        Debug.Log(IsGrounded() ? "isGrounded" : "isNotGrounded");
+
+        if (Input.GetKeyDown(KeyCode.Space) && IsGrounded())
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+    }
+
+    private bool IsGrounded()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, -Vector2.up, .6f, LayerMask.GetMask("Ground"));
+
+        return hit.collider;
     }
 }
