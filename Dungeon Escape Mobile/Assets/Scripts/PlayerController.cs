38c9fc50c7437c70b00b8ae33fc36be5af518197
LBruner <leandrobruner15@gmail.com>
2021-07-03 16:08:51 -0300

    video 8-9

---
+++ Dungeon Escape Mobile/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D rb = null;
+
+    [SerializeField] float speed = 2f;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+
+        rb.velocity = new Vector2(horizontalInput, rb.velocity.y) * speed * Time.deltaTime;
+    }
+}
