48359b4ab97aed019917d6a6c5abc5999220075c
LBruner <leandrobruner15@gmail.com>
2021-07-05 12:53:05 -0300

    11-12-13

---
+++ Dungeon Escape Mobile/Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Enemy : MonoBehaviour
+{
+    [SerializeField] protected Animator enemyAnimator = null;
+    [SerializeField] protected SpriteRenderer enemySprite = null;
+    [SerializeField] protected Transform pointA = null, pointB = null;
+
+    [SerializeField] protected int health;
+    [SerializeField] protected float speed;
+    [SerializeField] protected int gems;
+
+
+    private void Start()
+    {
+        
+    }
+
+    public abstract void FixedUpdate();
+
+}
