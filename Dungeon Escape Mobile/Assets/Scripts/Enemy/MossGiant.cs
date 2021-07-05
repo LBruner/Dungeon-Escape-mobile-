96f056543692e0d272c89b444df4d45a4491beb6
LBruner <leandrobruner15@gmail.com>
2021-07-05 12:53:05 -0300

    11-12-13-14

---
+++ Dungeon Escape Mobile/Assets/Scripts/Enemy/MossGiant.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MossGiant : Enemy
+{
+    Vector3 currentTarget;
+    private void Start()
+    {
+    }
+    public override void FixedUpdate()
+    {
+        if (IsIdle()) { return; }
+
+        Movement();
+    }
+
+    private void Movement()
+    {
+        if (transform.position.x < pointA.position.x +.2f)
+        {
+            currentTarget = pointB.position;
+            enemyAnimator.SetTrigger("idle");
+        }
+        else if (transform.position.x > pointB.position.x -.2f)
+        {
+            currentTarget = pointA.position;
+            enemyAnimator.SetTrigger("idle");
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, currentTarget, speed * Time.deltaTime);
+    }
+    
+    private bool IsIdle()
+    {
+        return enemyAnimator.GetCurrentAnimatorStateInfo(0).IsName("Idle");
+    }
+}
